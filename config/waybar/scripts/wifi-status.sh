2de4422f61833f597eee32bf8736335e418a7a17
carlosGomezMojica <gomezmojicacarlosmanuel@gmail.com>
2025-11-04 12:07:46 -0500

    feat: add window rules for blueman-manager and nm-connection-editor

--- config/waybar/scripts/wifi-status.sh
+++ config/waybar/scripts/wifi-status.sh
@@ -4,25 +4,25 @@
 wifi_status=$(nmcli -t -f active,ssid,signal device wifi list | grep yes)
 
 if [ -n "$wifi_status" ]; then
-    # Connected to Wi-Fi
-    ssid=$(echo "$wifi_status" | cut -d':' -f2)
-    signal=$(echo "$wifi_status" | cut -d':' -f3)
+  # Connected to Wi-Fi
+  ssid=$(echo "$wifi_status" | cut -d':' -f2)
+  signal=$(echo "$wifi_status" | cut -d':' -f3)
 
-    # Determine icon based on signal strength
-    if (( signal > 80 )); then
-        icon="󰤯" # Full signal
-    elif (( signal > 60 )); then
-        icon="󰤨" # High signal
-    elif (( signal > 40 )); then
-        icon="󰤥" # Medium signal
-    elif (( signal > 20 )); then
-        icon="󰤢" # Low signal
-    else
-        icon="󰤟" # Very low signal
-    fi
+  # Determine icon based on signal strength
+  if ((signal > 80)); then
+    icon="󰤯" # Full signal
+  elif ((signal > 60)); then
+    icon="󰤨" # High signal
+  elif ((signal > 40)); then
+    icon="󰤥" # Medium signal
+  elif ((signal > 20)); then
+    icon="󰤢" # Low signal
+  else
+    icon="󰤟" # Very low signal
+  fi
 
-    echo "{\"text\": \"${icon} ${ssid} (${signal}%) \", \"tooltip\": \"Connected to ${ssid} with ${signal}% signal\"}"
+  echo "{\"text\": \"${icon} (${signal}%) \", \"tooltip\": \"Connected to ${ssid} with ${signal}% signal\"}"
 else
-    # Not connected to Wi-Fi
-    echo "{\"text\": \"󰤭 Disconnected\", \"tooltip\": \"Not connected to Wi-Fi\"}"
+  # Not connected to Wi-Fi
+  echo "{\"text\": \"󰤭 \", \"tooltip\": \"Not connected to Wi-Fi\"}"
 fi
