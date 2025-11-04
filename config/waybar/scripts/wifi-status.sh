2ade314731ac55b8904650a08f5b2c401652c738
carlosGomezMojica <gomezmojicacarlosmanuel@gmail.com>
2025-11-04 12:00:41 -0500

    feat: refine waybar bluetooth icons

--- config/waybar/scripts/wifi-status.sh
+++ config/waybar/scripts/wifi-status.sh
@@ -7,8 +7,22 @@ if [ -n "$wifi_status" ]; then
     # Connected to Wi-Fi
     ssid=$(echo "$wifi_status" | cut -d':' -f2)
     signal=$(echo "$wifi_status" | cut -d':' -f3)
-    echo "{\"text\": \"${ssid} (${signal}%) \", \"tooltip\": \"Connected to ${ssid} with ${signal}% signal\"}"
+
+    # Determine icon based on signal strength
+    if (( signal > 80 )); then
+        icon="󰤯" # Full signal
+    elif (( signal > 60 )); then
+        icon="󰤨" # High signal
+    elif (( signal > 40 )); then
+        icon="󰤥" # Medium signal
+    elif (( signal > 20 )); then
+        icon="󰤢" # Low signal
+    else
+        icon="󰤟" # Very low signal
+    fi
+
+    echo "{\"text\": \"${icon} ${ssid} (${signal}%) \", \"tooltip\": \"Connected to ${ssid} with ${signal}% signal\"}"
 else
     # Not connected to Wi-Fi
-    echo "{\"text\": \"Disconnected\", \"tooltip\": \"Not connected to Wi-Fi\"}"
+    echo "{\"text\": \"󰤭 Disconnected\", \"tooltip\": \"Not connected to Wi-Fi\"}"
 fi
